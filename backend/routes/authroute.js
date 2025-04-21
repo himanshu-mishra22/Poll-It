f65090e84abce97f4037d23601cbb99d1775df36
HIMANSHU PRAKASH MISHRA <mhimanshuprakash@gmail.com>
2025-04-22 01:24:52 +0530

    minor issue fixed

--- backend/routes/authroute.js
+++ backend/routes/authroute.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const {registerUser, loginUser, getUSerInfo} = require('../controller/authController');
 const { protect } = require('../middlewares/authMiddleware');
+const {fs} = require("fs")
 const upload = require('../middlewares/uploadMiddleware');
 
 const router = express.Router();
@@ -14,7 +15,6 @@ router.post('/upload-image', upload.single('image'), (req,res)=>{
     if(!req.file){
         return res.status(400).json({message:"No file uploaded"});
     }
-
     const imgUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
     res.status(200).json({message:"File uploaded successfully", imgUrl});
     // console.log(req.file);
