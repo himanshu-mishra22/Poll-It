3b67232b3ef4738a0e2bdfc2fadb1d6419d878bf
HIMANSHU PRAKASH MISHRA <mhimanshuprakash@gmail.com>
2025-04-22 00:48:34 +0530

    fix: Update CORS origin to use environment variable for frontend URL

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 //middlewares
 app.use(cors({
-    origin:'https://poll-it-rust.vercel.app',
+    origin:process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
